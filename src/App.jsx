799af7495530b0177e398b5e19ee95590eea7943
h.h kayoum <kayoum096@gmail.com>
2024-11-10 13:49:59 +0600

    create movie data in js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import MovieList from "./cine/MovieList";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar></Sidebar>
+          <MovieList></MovieList>
+        </div>
+      </main>
     </>
   );
 }
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,85 +1,60 @@
-import Trending from './assets/icons/trending.svg';
-import NewRelease from './assets/icons/newRelease.svg';
-import ComingSoon from './assets/icons/commingSoon.svg';
-import Favourite from './assets/icons/favourite.svg';
-import WatchLater from './assets/icons/watchLater.svg';
-
+import commingSoon from "./assets/icons/commingSoon.svg";
+import favourite from "./assets/icons/favourite.svg";
+import newRelease from "./assets/icons/newRelease.svg";
+import trending from "./assets/icons/trending.svg";
+import watchLater from "./assets/icons/watchLater.svg";
 export default function Sidebar() {
-    return (
-        <aside>
-            <ul className="space-y-2">
-                <li>
-                    <a
-                        className="flex items-center space-x-2 px-5 py-3.5 rounded-lg bg-primary text-black"
-                        href="#"
-                    >
-                        <img
-                            src={Trending}
-                            width="24"
-                            height="24"
-                            alt=""
-                        />
-                        <span>Trending</span>
-                    </a>
-                </li>
-                <li>
-                    <a
-                        className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
-                        href="#"
-                    >
-                        <img
-                            src={NewRelease}
-                            width="24"
-                            height="24"
-                            alt=""
-                        />
-                        <span>New Releases</span>
-                    </a>
-                </li>
-                <li>
-                    <a
-                        className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
-                        href="#"
-                    >
-                        <img
-                            src={ComingSoon}
-                            width="24"
-                            height="24"
-                            alt=""
-                        />
-                        <span>Coming Soon</span>
-                    </a>
-                </li>
-                <li>
-                    <a
-                        className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
-                        href="#"
-                    >
-                        <img
-                            src={Favourite}
-                            width="24"
-                            height="24"
-                            alt=""
-                        />
-                        <span>Favourites</span>
-                    </a>
-                </li>
-                <li>
-                    <a
-                        className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
-                        href="#"
-                    >
-                        <img
-                            src={WatchLater}
-                            width="24"
-                            height="24"
-                            alt=""
-                        />
-                        <span>Watch Later</span>
-                    </a>
-                </li>
-            </ul>
-        </aside>
-    );
+  return (
+    <>
+      <aside>
+        <ul className="space-y-2">
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg bg-primary text-black"
+              href="#"
+            >
+              <img src={trending} width="24" height="24" alt="trending" />
+              <span>Trending</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img src={newRelease} width="24" height="24" alt="newRelease" />
+              <span>New Releases</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img src={commingSoon} width="24" height="24" alt="commingSoon" />
+              <span>Coming Soon</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img src={favourite} width="24" height="24" alt="favourite" />
+              <span>Favourites</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img src={watchLater} width="24" height="24" alt="watchLater" />
+              <span>Watch Later</span>
+            </a>
+          </li>
+        </ul>
+      </aside>
+    </>
+  );
 }
-
