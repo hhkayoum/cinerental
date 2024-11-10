a3d94532c761e965aeec929dab48385743a64506
h.h kayoum <kayoum096@gmail.com>
2024-11-10 15:15:31 +0600

    render movieList in ui

--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,24 +1,33 @@
-export default function MovieCard() {
+/* eslint-disable react/prop-types */
+import { getImgUrl } from "../utils/cine-utility";
+
+export default function MovieCard({ movie }) {
   return (
     <>
       <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-        <a href="#">
-          <img className="w-full object-cover" src="" alt="" />
-          <figcaption className="pt-4">
-            <h3 className="text-xl mb-1">title</h3>
-            <p className="text-[#575A6E] text-sm mb-2">genre</p>
-            <div className="flex items-center space-x-1 mb-5">
-              {/* <Rating /> */}
-            </div>
-            <button
-              className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-              href="#"
-            >
-              <img src="./assets/tag.svg" alt="" />
-              <span>$price | Add to Cart</span>
-            </button>
-          </figcaption>
-        </a>
+        <img
+          className="w-full object-cover"
+          src={getImgUrl(movie.cover)}
+          alt=""
+        />
+        <figcaption className="pt-4">
+          <h3 className="text-xl mb-1">{movie.title}</h3>
+          <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+          <div className="flex items-center space-x-1 mb-5">
+            <img src="./assets/star.svg" width="14" height="14" alt="" />
+            <img src="./assets/star.svg" width="14" height="14" alt="" />
+            <img src="./assets/star.svg" width="14" height="14" alt="" />
+            <img src="./assets/star.svg" width="14" height="14" alt="" />
+            <img src="./assets/star.svg" width="14" height="14" alt="" />
+          </div>
+          <a
+            className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+            href="#"
+          >
+            <img src="./assets/tag.svg" alt="" />
+            <span>$100 | Add to Cart</span>
+          </a>
+        </figcaption>
       </figure>
     </>
   );
--- src/cine/Rating.jsx
+++ src/cine/Rating.jsx
@@ -1,20 +1,13 @@
-import Star from '../assets/star.svg';
+/* eslint-disable react/prop-types */
+import Star from "../assets/star.svg";
 
-export default function Rating({value}) {
-    const stars = Array(value).fill(Star);
-    return(
-        <>
-            {
-                stars.map((star, index) => (
-                    <img
-                        key={index}
-                        src={star}
-                        width="14"
-                        height="14"
-                        alt="star"
-                    />
-                ))
-            }
-        </>
-    )
+export default function Rating({ value }) {
+  const stars = Array(value).fill(Star);
+  return (
+    <>
+      {stars.map((star, index) => (
+        <img key={index} src={star} width="14" height="14" alt="star" />
+      ))}
+    </>
+  );
 }
