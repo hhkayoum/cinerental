04d572553188c9f7c2fdd58112419d3629db6a29
h.h kayoum <kayoum096@gmail.com>
2024-11-10 15:38:36 +0600

    add modal open/close functionality with dynamic movie data

--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,9 +1,28 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import { getImgUrl } from "../utils/cine-utility";
+import MovieDetailsModal from "./MovieDetailsModal";
+import Rating from "./Rating";
 
 export default function MovieCard({ movie }) {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  function handleCloseModal() {
+    setSelectedMovie(null);
+    setShowModal(false);
+  }
+  function handleMovieSelection(movie) {
+    setSelectedMovie(movie);
+    setShowModal(true);
+  }
   return (
     <>
+      {showModal && (
+        <MovieDetailsModal
+          movie={selectedMovie}
+          onClose={handleCloseModal}
+        ></MovieDetailsModal>
+      )}
       <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
         <img
           className="w-full object-cover"
@@ -14,13 +33,10 @@ export default function MovieCard({ movie }) {
           <h3 className="text-xl mb-1">{movie.title}</h3>
           <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
           <div className="flex items-center space-x-1 mb-5">
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
+            <Rating value={movie.rating}></Rating>
           </div>
           <a
+            onClick={() => handleMovieSelection(movie)}
             className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
             href="#"
           >
